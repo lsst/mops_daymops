495bc20ac2223f0a9e679b38ce34281ec409f8c1
fpierfed <fpierfed@git.lsstcorp.org>
2009-08-11 17:51:40 +0000

    Implemented generic getId() method.

--- python/lsst/mops/daymops/DayMOPSObject.py
+++ python/lsst/mops/daymops/DayMOPSObject.py
@@ -18,3 +18,44 @@ class DayMOPSObject(object):
             setattr(self.__class__, name, setter)
             return(getattr(self, name))
         raise(AttributeError(name))
+    
+    # Implement simple default getId() method.
+    def getId(self):
+        # Try and understand which attribute is an id. Usually it is an 
+        # attribute of the form <className>[0].lower() + <className>[1:] + 'Id'
+        # so the method we want is 'get' + <className> + 'Id'()
+        methodName = 'get%sId' %(self.__class__.__name__)
+        if(hasattr(self, methodName)):
+            return(getattr(self, methodName)())
+        return(None)
+    
+    # Default comparison is by id.
+    def __lt__(self, other):
+        if(other == None):
+            return(False)
+        return(self.getId() < other.getId())
+    
+    def __le__(self, other):
+        if(other == None):
+            return(False)
+        return(self.getId() <= other.getId())
+    
+    def __eq__(self, other):
+        if(other == None):
+            return(False)
+        return(self.getId() == other.getId())
+    
+    def __ne__(self, other):
+        if(other == None):
+            return(True)
+        return(self.getId() != other.getId())
+
+    def __gt__(self, other):
+        if(other == None):
+            return(False)
+        return(self.getId() > other.getId())
+    
+    def __ge__(self, other):
+        if(other == None):
+            return(False)
+        return(self.getId() >= other.getId())

589e67a5fdf6cf8318aeb904a79abf851f032f79
jmyers <jmyers@git.lsstcorp.org>
2011-02-15 01:54:03 +0000

    Updated makeLinkTrackletsRunScripts to create C++ runscripts as well as C.

--- tests/experimentScripts/makeLinkTrackletsRunScripts.py
+++ tests/experimentScripts/makeLinkTrackletsRunScripts.py
@@ -7,15 +7,20 @@ import os
 
 INFILES_GLOB=glob.glob("../*.miti")
 START_T_RANGE_FOR_INFILE=lambda inf: inf[:-4] + "start_t_range"
+CPP_START_T_RANGE_FOR_INFILE=lambda inf: inf[:-4] + "date.start_t_range"
+CPP_DETS_FOR_INFILE=lambda inf: inf[:-4] + ".dets"
+CPP_IDS_FOR_INFILE=lambda inf: inf[:-4] + ".ids"
+CPP_TRACKS_FOR_INFILE=lambda inf: inf[:-4] + ".cpp.tracks"
+CPP_OUTF_FOR_INFILE=lambda inf: os.path.basename(inf[:-4] + ".cpp.cmd.sh")
+
 BASENAME_FOR_INFILE=lambda inf: os.path.basename(inf[:-5])
-OUTF_FOR_INFILE=lambda inf: os.path.basename(inf[:-4] + "cmd.sh")
+OUTF_FOR_INFILE=lambda inf: os.path.basename(inf[:-4] + "c.cmd.sh")
 
 
 def writeRunScript(infile, startTRangeFile):
     outf = file(OUTF_FOR_INFILE(infile),'w')
     outS = """#!/usr/bin/bash
 
-# a quick test to determine the approximate runtime/num false tracklets from a run with min_sup == 2 and 5 day windows
 
 BN=""" + BASENAME_FOR_INFILE(infile) + """
 
@@ -28,6 +33,27 @@ echo Running: $CMD
     outf.write(outS)
     outf.close()
 
+
+def writeCppRunScript(infile, startTRangeFile):
+    outf = file(CPP_OUTF_FOR_INFILE(infile),'w')
+    dets = CPP_DETS_FOR_INFILE(infile)
+    ids = CPP_IDS_FOR_INFILE(infile)
+    tracks = CPP_TRACKS_FOR_INFILE(infile)
+    args = "-d " + dets + " -t " + ids + " -o " + tracks
+
+    outS = """#!/usr/bin/bash
+
+
+CMD="$MOPS_DAYMOPS_DIR/bin/linkTracklets """ + args + """
+
+echo Running: $CMD
+
+/usr/bin/time -o $BN.cpp.linkTracklets.runtime $CMD | tee $BN.cpp.linkTracklets.runlog
+"""
+    outf.write(outS)
+    outf.close()
+
+
 if __name__=="__main__":
     for infile in INFILES_GLOB:
         print infile
@@ -35,3 +61,4 @@ if __name__=="__main__":
         print startTRangeFile
 
         writeRunScript(infile, startTRangeFile)
+        writeCppRunScript(infile, startTRangeFile)
